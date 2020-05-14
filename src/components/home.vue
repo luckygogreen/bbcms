ec596c293b8fad58e30bbd3d0bc5b3614b90193b
luckygogreen <luckygogreen@users.noreply.github.com>
2020-05-13 22:54:27 -0400

    init home page

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,8 +1,35 @@
 <template>
     <el-container class="home-container">
-        <el-aside width="200px">Aside</el-aside>
+        <el-aside width="200px">
+            <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+                <!--                一级导航-->
+                <el-submenu index="1">
+                    <!--                    一级导航模板-->
+                    <template slot="title">
+                        <!--                        一级导航图标-->
+                        <i class="el-icon-location"></i>
+                        <!--                        一级导航文本-->
+                        <span>导航一</span>
+                    </template>
+                    <!--                    二级导航-->
+                    <el-menu-item index="1-1">
+                        <!--                        二级导航模板-->
+                        <template slot="title">
+                            <!--                            二级导航图标-->
+                            <i class="el-icon-location"></i>
+                            <!--                            二级导航文本-->
+                            <span>导航一</span>
+                        </template>
+                    </el-menu-item>
+                </el-submenu>
+            </el-menu>
+        </el-aside>
         <el-container>
             <el-header>
+                <div>
+                    <img class="logo-class" src="../assets/logo.png" alt="">
+                    <span>Vue + Element Ui 后台管理</span>
+                </div>
                 <el-button type="info" @click="logout">退出</el-button>
             </el-header>
             <el-main>Main</el-main>
@@ -27,19 +54,62 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.logo-class {
+    margin-left: 15px;
+    width: 10%;
+    height: 10%;
+}
+
 .home-container {
     height: 100%;
 }
-.el-container {
-    background-color: #999999;
-}
-.el-header {
-    background-color: #2b4b6b;
-}
-.el-aside {
-    background-color: #333333;
-}
+
 .el-footer {
-    background-color: #2b4b6b;
+    background-color: #373d41;
+}
+
+.el-header {
+    background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+
+    > div {
+        display: flex;
+        align-items: center;
+
+        span {
+            margin-left: 15px;
+        }
+    }
+}
+
+.el-aside {
+    background-color: #333744;
+
+    .el-menu {
+        border-right: none;
+    }
+}
+
+.el-main {
+    background-color: #eaedf1;
+}
+
+.iconfont {
+    margin-right: 10px;
+}
+
+.toggle-button {
+    background-color: #4a5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;
+    cursor: pointer;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Notification, Message, Container, Header, Aside, Main, Footer } from 'element-ui'
+import {
+  Button,
+  Form,
+  FormItem,
+  Input,
+  Notification,
+  Message,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Footer,
+  Menu,
+  MenuItem,
+  Submenu
+} from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -10,5 +25,8 @@ Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
 Vue.use(Footer)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(Submenu)
 Vue.prototype.$notify = Notification // 把 Notification 挂载到 vue的原型对象上 挂载后的原型对象编可以通过 this.$ 来操作
 Vue.prototype.$message = Message // 把 Notification 挂载到 vue的原型对象上 挂载后的原型对象编可以通过 this.$ 来操作
